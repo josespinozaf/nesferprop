635d4d0439816eb0fa74ac8aaf88bf31b032c5d7
José Espinoza <josespinoza@alumnos.uai.cl>
2016-04-30 18:27:56 -0300

    DB integrada en properties_listing_list

---
+++ php/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "nestorfe_prop";
+$db = mysql_connect($dbhost,$dbuser,$dbpass) or die('No se puede conectar a la base de datos.');
+mysql_select_db($dbname) or die('Problema al seleccionar la base de datos.');
+?>
