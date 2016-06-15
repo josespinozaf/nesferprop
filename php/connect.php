501e99ae13fffab49dff736b867e3bfe16f0664a
José Espinoza <josespinoza@alumnos.uai.cl>
2016-06-15 12:47:58 -0400

    charset 'utf8'

--- php/connect.php
+++ php/connect.php
@@ -6,4 +6,5 @@ $dbpass = "Pagina123=";
 $dbname = "nestorfe_prop";
 $db = mysql_connect($dbhost,$dbuser,$dbpass) or die('No se puede conectar a la base de datos.');
 mysql_select_db($dbname) or die('Problema al seleccionar la base de datos.');
+mysql_set_charset ('utf8',$db);
 ?>
