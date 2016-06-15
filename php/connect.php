ce8d6fe5c174025c08242edc81359f46c5dcbe09
José Espinoza <josespinoza@alumnos.uai.cl>
2016-06-15 14:00:57 -0400

    connect comentado

--- php/connect.php
+++ php/connect.php
@@ -1,15 +1,13 @@
 <?php
-<<<<<<< HEAD
+
 
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-=======
 //header("Content-Type: text/html;charset=utf-8");
-$dbhost = "localhost:3306";
-$dbuser = "nestorfe";
-$dbpass = "Pagina123=";
->>>>>>> origin/master
+//$dbhost = "localhost:3306";
+//$dbuser = "nestorfe";
+//$dbpass = "Pagina123="; origin/master
 $dbname = "nestorfe_prop";
 $db = mysql_connect($dbhost,$dbuser,$dbpass) or die('No se puede conectar a la base de datos.');
 mysql_select_db($dbname) or die('Problema al seleccionar la base de datos.');
