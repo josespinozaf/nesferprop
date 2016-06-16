d656d47c2a74147c66e61769d74cf8161cdac05f
fermandoa <di.vi.fernandez.b@gmail.com>
2016-06-15 22:20:41 -0400

    localhost, root

--- php/connect.php
+++ php/connect.php
@@ -1,13 +1,13 @@
 <?php
 
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
+//$dbhost = "localhost";
+//$dbuser = "root";
+//$dbpass = "";
 //header("Content-Type: text/html;charset=utf-8");
-//$dbhost = "localhost:3306";
-//$dbuser = "nestorfe";
-//$dbpass = "Pagina123="; 
+$dbhost = "localhost:3306";
+$dbuser = "nestorfe";
+$dbpass = "Pagina123="; 
 $dbname = "nestorfe_prop";
 $db = mysql_connect($dbhost,$dbuser,$dbpass) or die('No se puede conectar a la base de datos.');
 mysql_select_db($dbname) or die('Problema al seleccionar la base de datos.');
